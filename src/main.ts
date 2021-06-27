16e7b748b16deb0f16512b72f1043423ca91a1d2
BruceKangCN <brucekangcn@gmail.com>
2021-06-27 18:38:28 +0800

    add tip for setting local environments

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,13 @@ import App from './App.vue';
 import router from './router';
 
 createApp(App).use(router).mount('#app');
+
+// 在非生产环境下提示设置本地环境变量
+if (process.env.NODE_ENV !== 'production') {
+  console.warn([
+    `You are in ${process.env.NODE_ENV} mode.`,
+    `Remember to set the variables below in your .env.${process.env.NODE_ENV}.local file in the root directory,`,
+    'and DON\'T FORGET to restart the service after making changes:',
+    `    VUE_APP_REST_API_URL(${process.env.VUE_APP_REST_API_URL}): the REST API url for process engine`,
+  ].join('\n'));
+}
